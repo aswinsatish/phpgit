6730f15a0f825d069c216223a0aa0f765539ddf3
logical99 <vish@logicalsteps.net>
2018-01-27 15:36:23 +0530

    Random Update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World from PHP running via Captain-Git-Deploy!!!</p>'; ?> 
+ <?php echo '<p>Hello World from PHP running via Testing Captain-Git-Deploy!!!</p>'; ?> 
  </body>
 </html>
